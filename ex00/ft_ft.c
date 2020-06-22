f88e51b5fd6781c2498a7dfce6dd076375a91218
Mxolisi Shiba <mshiba@c4r15s5.wethinkcode.co.za>
2020-06-22 10:56:22 +0200

    origin

---
+++ ex00/ft_ft.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <unistd.h>
+
+void print_ft_ft(int *nbr)
+{ if (nbr)
+	{
+		*nbr = 42;
+	}
+}
+int main(void)
+{
+	putnbr();
+		return(0);
+}
---
+++ ex01/ft_ultimate_ft.c
@@ -0,0 +1,9 @@
+#include <unistd.h>
+
+void ft_ultimate_ft(int ***nbr)
+{
+	if (nbr)
+	{
+		***nbr = 42;
+	}
+}
