5d50e26437967a968c0c2ab09aa772b91a0134a4
Mxolisi Shiba <mshiba@c4r15s5.wethinkcode.co.za>
2020-06-25 12:40:03 +0200

    origin

--- ex01/main.c
+++ ex01/main.c
@@ -1,19 +1,30 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_ultimate_ft.c                                   :+:      :+:    :+:   */
+/*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mshiba <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/06/24 14:38:54 by mshiba            #+#    #+#             */
-/*   Updated: 2020/06/25 12:33:29 by mshiba           ###   ########.fr       */
+/*   Created: 2020/06/25 12:33:48 by mshiba            #+#    #+#             */
+/*   Updated: 2020/06/25 12:39:18 by mshiba           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-#include <unistd.h>
 #include <stdio.h>
+#include <unistd.h>
 
-void ft_ultimate_ft(int *********nbr ){
-	
-	 *********nbr = 42;
-}
+int main()
+{
+	int a = 0;
+	int *p1 = n;
+    int **p2 = p1;
+	int ***p3 = p2;
+	int ****p4 = p3;
+	int *****p5 = p4;
+	int ******p6 = p5;
+	int *******p7 = p6;
+	int ********p8 = p7;
+
+	ft_ultimate-ft(&p8);
+	printf("%d\n" , a);
+    return 0;
 
